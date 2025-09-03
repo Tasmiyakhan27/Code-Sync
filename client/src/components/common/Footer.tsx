93a0442bd80df8ed2eafa1c981eb88cddc8941d4
Tasmiyakhan27 <pathan.tasmiya27@gmail.com>
2025-09-03 20:24:08 +0530

    Initial commit

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/Tasmiyakhan27"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Tasmiyakhan27
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
